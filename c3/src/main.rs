d6d28dfbe84dc4cf3326a65c9e61d307be25306b
Coba <coba@cobac.eu>
2022-10-05 23:05:03 +0200

    c3 data types

--- c3/src/main.rs
+++ c3/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //  Variables
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
@@ -26,5 +27,27 @@ fn main() {
     // let mut spaces = "   ";
     // spaces = spaces.len();
 
+    // Data types
+
     println!("Spaces is: {spaces}");
+    //let x: u32 = 5;
+    // let y = x - 10; Compiler caches it, overflows
+
+    let floored = 2 / 3; // => 0, round to nearest int
+    println!("floored = {floored}");
+
+    let power = i32::pow(2, 3);
+    println!("power = {power}");
+
+    let tup = (1, 2, 3);
+    let (tup1, tup2, tup3) = tup;
+
+    println!("Tuple values: {tup1}, {tup2}, {tup3}.");
+
+    let tup0 = tup.0;
+    println!("Rust uses 0-indexing, as tup0 is = {tup0}.");
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    // Printing via debug, array does not implement a default fomater
+    println!("a = {a:?}.");
 }
