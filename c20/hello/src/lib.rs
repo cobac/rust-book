1db907ba8c6fe31b9b75132e031a762afd646941
Coba <coba@cobac.eu>
2023-07-30 15:30:11 +0200

    c20 done

--- c20/hello/src/lib.rs
+++ c20/hello/src/lib.rs
@@ -5,27 +5,38 @@ use std::{
 
 struct Worker {
     id: usize,
-    handle: thread::JoinHandle<()>,
+    handle: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
         let handle = thread::spawn(move || loop {
-            let job = receiver
+            let message = receiver
                 .lock()
                 .expect("A thread holding the mutex lock did a panic")
-                .recv()
-                .expect("Sender thread is down");
-            println!("Worker {id} got a job; executing.");
-            job();
+                .recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} is disconnected; a mimir.");
+                    break;
+                }
+            }
         });
-        Self { id, handle }
+        Self {
+            id,
+            handle: Some(handle),
+        }
     }
 }
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -48,13 +59,28 @@ impl ThreadPool {
         for worker_id in 0..size {
             workers.push(Worker::new(worker_id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(handle) = worker.handle.take() {
+                handle.join().unwrap()
+            }
+        }
     }
 }
